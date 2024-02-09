c7019ed6ee5be7f353421f16d493af7eb5e2e113
TacticalLaptopBag <lightpower26@hotmail.com>
2024-02-09 16:40:34 -0500

    Cleaned up interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,18 @@
 import {
-  ButtonItem,
   definePlugin,
-  DialogButton,
-  Menu,
-  MenuItem,
-  Navigation,
   PanelSection,
   PanelSectionRow,
   ServerAPI,
-  showContextMenu,
   staticClasses,
   ToggleField,
 } from "decky-frontend-lib";
 import { useState, VFC } from "react";
 import { FaShip } from "react-icons/fa";
 
-import logo from "../assets/logo.png";
-
-// interface AddMethodArgs {
-//   left: number;
-//   right: number;
-// }
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
-  // const [result, setResult] = useState<number | undefined>();
-
   const [isEnabled, setEnabled] = useState<boolean>(true);
   let changingEnabled = false;
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
 
   serverAPI.callPluginMethod<{}, boolean>("is_enabled", {}).then((response) => {
     if(response.success) {
@@ -47,10 +21,10 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
   })
 
   return (
-    <PanelSection title="Panel Section">
+    <PanelSection>
       <PanelSectionRow>
-      This is where the enabled switch goes
       <ToggleField
+        label="Disable Deck Controller while External Controller connected"
         checked={isEnabled}
         onChange={(value: boolean) => {
           if(changingEnabled) return
@@ -71,70 +45,34 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
           }
         }}
       >
-        Enabled
       </ToggleField>
-      </PanelSectionRow>
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={(e) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
-          }
-        >
-          Server says yolo
-        </ButtonItem>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Navigation.CloseSideMenus();
-            Navigation.Navigate("/decky-nav-test");
-          }}
-        >
-          Router
-        </ButtonItem>
       </PanelSectionRow>
     </PanelSection>
   );
 };
 
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Navigation.NavigateToLibraryTab()}>
-        Go to Library
-      </DialogButton>
-    </div>
-  );
-};
+// const DeckyPluginRouterTest: VFC = () => {
+//   return (
+//     <div style={{ marginTop: "50px", color: "white" }}>
+//       Hello World!
+//       <DialogButton onClick={() => Navigation.NavigateToLibraryTab()}>
+//         Go to Library
+//       </DialogButton>
+//     </div>
+//   );
+// };
 
 export default definePlugin((serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-nav-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
+  // serverApi.routerHook.addRoute("/decky-nav-test", DeckyPluginRouterTest, {
+  //   exact: true,
+  // });
 
   return {
     title: <div className={staticClasses.Title}>Controller Dock</div>,
     content: <Content serverAPI={serverApi} />,
     icon: <FaShip />,
-    onDismount() {
-      serverApi.routerHook.removeRoute("/decky-nav-test");
-    },
+    // onDismount() {
+    //   serverApi.routerHook.removeRoute("/decky-nav-test");
+    // },
   };
 });
