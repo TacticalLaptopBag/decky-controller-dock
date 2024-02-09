4fbc6fbd5b216946bbd8980632cb5376ab7926e8
TacticalLaptopBag <lightpower26@hotmail.com>
2024-02-09 16:08:05 -0500

    install/uninstall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from urllib.request import urlretrieve
+# from urllib.request import urlretrieve
 
 # The decky plugin module is located at decky-loader/plugin
 # For easy intellisense checkout the decky-loader code one directory up
@@ -8,10 +8,10 @@ from urllib.request import urlretrieve
 import decky_plugin
 
 # scawp's repo
-# REPO_URL = "https://raw.githubusercontent.com/scawp/Steam-Deck.Auto-Disable-Steam-Controller/main"
+# REPO_URL = "http://raw.githubusercontent.com/scawp/Steam-Deck.Auto-Disable-Steam-Controller/main"
 
 # Mazebird's repo
-REPO_URL = "https://raw.githubusercontent.com/Mazebird/Steam-Deck.Auto-Disable-Steam-Controller/patch-1"
+REPO_URL = "http://raw.githubusercontent.com/Mazebird/Steam-Deck.Auto-Disable-Steam-Controller/patch-1"
 
 # Dirs
 TMP_DIR = "/tmp/scawp.SDADSC.install"
@@ -20,33 +20,38 @@ SCRIPT_INSTALL_DIR = "/home/deck/.local/share/scawp/SDADSC"
 SCRIPT_CONF_DIR = f"{SCRIPT_INSTALL_DIR}/conf"
 
 SCRIPT_DEST_PATH = f"{SCRIPT_INSTALL_DIR}/disable_steam_input.sh"
-DEVICE_LIST_DEST_PATH = f"{SCRIPT_INSTALL_DIR}/simple_device_list.txt"
+DEVICE_LIST_DEST_PATH = f"{SCRIPT_CONF_DIR}/simple_device_list.txt"
 RULES_DEST_PATH = f"{RULES_INSTALL_DIR}/99-disable-steam-input.rules"
 DISABLED_PATH = f"{SCRIPT_CONF_DIR}/disabled"
 
-class Plugin:
-    enabled: bool = True
+enabled = not os.path.exists(DISABLED_PATH)
 
+initialized = False
+
+class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
     async def add(self, left, right):
         return left + right
 
-    def _download_file(self, file_name):
+    def _download_file(file_name):
         url = f"{REPO_URL}/{file_name}"
-        decky_plugin.logger.debug(f"Downloading {url}")
-        return urlretrieve(url, f"{TMP_DIR}/{file_name}")[0]
+        dest = f"{TMP_DIR}/{file_name}"
+        decky_plugin.logger.info(f"Downloading {url}")
+        # return urlretrieve(url, f"{TMP_DIR}/{file_name}")[0]
+        os.system(f"curl -o \"{dest}\" \"{url}\"")
+        return dest
 
-    def _remove_path(self, path):
+    def _remove_path(path):
         if os.path.isdir(path):
             shutil.rmtree(path)
         elif os.path.isfile(path):
             os.remove(path)
 
     async def is_enabled(self):
-        return self.enabled
+        global enabled
+        return enabled
 
-    async def install(self):
-
+    def install():
         decky_plugin.logger.info("Installing scawp's script...")
 
         if os.path.exists(SCRIPT_DEST_PATH):
@@ -56,18 +61,13 @@ class Plugin:
         decky_plugin.logger.info(f"Making tmp directory {TMP_DIR}")
         if os.path.exists(TMP_DIR):
             decky_plugin.logger.warn("Found existing tmp directory! Did previous installation fail?")
-            if os.path.isdir(TMP_DIR):
-                shutil.rmtree(TMP_DIR)
-            else:
-                os.remove(TMP_DIR)
-            decky_plugin.logger.info("Removed existing tmp directory")
         
         os.mkdir(TMP_DIR)
         
         decky_plugin.logger.info("Downloading files...")
-        script_path = self._download_file("disable_steam_input.sh")
-        device_list_path = self._download_file("simple_device_list.txt")
-        rules_path = self._download_file("99-disable-steam-input.rules")
+        script_path = Plugin._download_file("disable_steam_input.sh")
+        device_list_path = Plugin._download_file("simple_device_list.txt")
+        rules_path = Plugin._download_file("99-disable-steam-input.rules")
 
         decky_plugin.logger.info(f"Making script directory {SCRIPT_INSTALL_DIR}")
         os.makedirs(SCRIPT_CONF_DIR, exist_ok=True)
@@ -81,8 +81,12 @@ class Plugin:
         decky_plugin.logger.info("Setting script permissions")
         os.chmod(SCRIPT_DEST_PATH, 0o555)
 
-        decky_plugin.logger.info(f"Copying {rules_path} to {rules_dest_path}")
-        shutil.copy2(rules_path, rules_dest_path)
+        decky_plugin.logger.info(f"Copying {rules_path} to {RULES_DEST_PATH}")
+        shutil.copy2(rules_path, RULES_DEST_PATH)
+
+        decky_plugin.logger.info("Setting file ownership")
+        os.system(f"chown 1000:1000 -R {SCRIPT_INSTALL_DIR}")
+        os.system(f"chown 1000:1000 -R {SCRIPT_INSTALL_DIR}")
 
         decky_plugin.logger.info("Reloading udevadm")
         os.system("udevadm control --reload")
@@ -94,14 +98,14 @@ class Plugin:
         return True
 
 
-    async def uninstall(self):
+    def uninstall(self):
         decky_plugin.logger.info("Uninstalling scawp's script...")
 
         decky_plugin.logger.info(f"Removing {SCRIPT_INSTALL_DIR}")
-        self._remove_path(SCRIPT_INSTALL_DIR)
+        Plugin._remove_path(SCRIPT_INSTALL_DIR)
 
         decky_plugin.logger.info(f"Removing {RULES_DEST_PATH}")
-        self._remove_path(RULES_DEST_PATH)
+        Plugin._remove_path(RULES_DEST_PATH)
 
         decky_plugin.logger.info("Reloading udevadm")
         os.system("udevadm control --reload")
@@ -111,47 +115,39 @@ class Plugin:
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
+        global initialized;
+        if initialized:
+            return
+
+        initialized = True
+
         decky_plugin.logger.info("Hello World!")
-        await self.install()
-        self.enabled = not os.path.exists(DISABLED_PATH)
+        Plugin.install()
 
     # Function called first during the unload process, utilize this to handle your plugin being removed
     async def _unload(self):
         decky_plugin.logger.info("Goodbye World!")
-        self.uninstall()
-        pass
+        Plugin.uninstall(self)
 
     async def disable(self):
+        global enabled
         decky_plugin.logger.info("Disabling scawp's script...")
         # Create disabled file
         with open(DISABLED_PATH, "w") as file:
+            os.system(f"chown 1000:1000 {DISABLED_PATH}")
             pass
 
-        self.enabled = False
+        enabled = False
         decky_plugin.logger.info("Disabled scawp's script")
 
     async def enable(self):
+        global enabled
         decky_plugin.logger.info("Enabling scawp's script...")
-        self._remove_path(DISABLED_PATH)
-        self.enabled = True
+        Plugin._remove_path(DISABLED_PATH)
+        enabled = True
         decky_plugin.logger.info("Enabled scawp's script")
 
     # Migrations that should be performed before entering `_main()`.
     async def _migration(self):
         decky_plugin.logger.info("Migrating")
-        # Here's a migration example for logs:
-        # - `~/.config/decky-template/template.log` will be migrated to `decky_plugin.DECKY_PLUGIN_LOG_DIR/template.log`
-        decky_plugin.migrate_logs(os.path.join(decky_plugin.DECKY_USER_HOME,
-                                               ".config", "decky-template", "template.log"))
-        # Here's a migration example for settings:
-        # - `~/homebrew/settings/template.json` is migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/template.json`
-        # - `~/.config/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_SETTINGS_DIR/`
-        decky_plugin.migrate_settings(
-            os.path.join(decky_plugin.DECKY_HOME, "settings", "template.json"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".config", "decky-template"))
-        # Here's a migration example for runtime data:
-        # - `~/homebrew/template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        # - `~/.local/share/decky-template/` all files and directories under this root are migrated to `decky_plugin.DECKY_PLUGIN_RUNTIME_DIR/`
-        decky_plugin.migrate_runtime(
-            os.path.join(decky_plugin.DECKY_HOME, "template"),
-            os.path.join(decky_plugin.DECKY_USER_HOME, ".local", "share", "decky-template"))
+        
