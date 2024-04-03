f70b05b08a816663858d59268670f589d44fc531
TacticalLaptopBag <lightpower26@hotmail.com>
2024-04-03 13:11:49 -0400

    Created toggles for controller, keyboard, and mouse functionality

--- main.py
+++ main.py
@@ -5,28 +5,68 @@ import os
 # or add the `decky-loader/plugin` path to `python.analysis.extraPaths` in `.vscode/settings.json`
 import decky_plugin
 
+plugin_path = os.path.dirname(os.path.realpath(__file__))
+script_path = os.path.join(plugin_path, "Steam-Deck.Auto-Disable-Steam-Controller", "disable_steam_input.sh")
+rules_dest_dir = "/etc/udev/rules.d"
+rules = {
+    "controller": os.path.join(rules_dest_dir, "99-disable-steam-input_controller.rules"),
+    "keyboard": os.path.join(rules_dest_dir, "99-disable-steam-input_keyboard.rules"),
+    "mouse": os.path.join(rules_dest_dir, "99-disable-steam-input_mouse.rules")
+}
+
 
 class Plugin:
+    async def get_state(self):
+        return [os.path.exists(rules["controller"]), os.path.exists(rules["keyboard"]), os.path.exists(rules["mouse"])]
+
+
+    async def enable_functionality(self, functionality):
+        decky_plugin.logger.info(f"Enabling {functionality} functionality...")
+        try:
+            # This is incredibly cursed. For some reason, `self` needs to be given as an argument.
+            # Is Decky not instantiating an instance of their plugin backends?
+            self.write_rules(self, functionality)
+            decky_plugin.logger.info(f"Enabled {functionality} functionality")
+        except BaseException as e:
+            decky_plugin.logger.error(f"Failed to enable {functionality} functionality!")
+            decky_plugin.logger.error(str(e))
+
+
+    async def disable_functionality(self, functionality):
+        decky_plugin.logger.info(f"Disabling {functionality} functionality...")
+        try:
+            os.remove(rules[functionality])
+            decky_plugin.logger.info(f"Disabled {functionality} functionality")
+        except BaseException as e:
+            decky_plugin.logger.error(f"Failed to disable {functionality} functionality!")
+            decky_plugin.logger.error(str(e))
+
+
+    def write_rules(self, functionality):
+        decky_plugin.logger.info(f"Writing steam input {functionality} rules...")
+        with open(rules[functionality], "w") as rules_file:
+            # echo 'KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="1", ACTION=="add", RUN+="/home/deck/.local/share/scawp/SDADSC/disable_steam_input.sh disable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"' | sudo tee -a "$rules_install_dir/99-disable-steam-input.rules"
+            # echo 'KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="1", ACTION=="remove", RUN+="/home/deck/.local/share/scawp/SDADSC/disable_steam_input.sh enable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"' | sudo tee -a "$rules_install_dir/99-disable-steam-input.rules"
+            id_var = functionality.upper() if functionality != "controller" else "JOYSTICK"
+            rules_file.write('KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_'+id_var+'}=="1", ACTION=="add", RUN+="\''+script_path+'\' disable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"')
+            rules_file.write("\n")
+            rules_file.write('KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_'+id_var+'}=="1", ACTION=="remove", RUN+="\''+script_path+'\' enable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"')
+            rules_file.write("\n")
+        decky_plugin.logger.info(f"Wrote steam input {functionality} rules to {rules[functionality]}")
+
+        self.reload_services(self)
+
+
+    def reload_services(self):
+        decky_plugin.logger.info("Reloading services...")
+        os.system("udevadm control --reload")
+        decky_plugin.logger.info("Reloaded services")
+
+
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         decky_plugin.logger.info("Hello World!")
-        rules_dest_dir = "/etc/udev/rules.d"
-        plugin_path = os.path.dirname(os.path.realpath(__file__))
-        script_path = os.path.join(plugin_path, "Steam-Deck.Auto-Disable-Steam-Controller", "disable_steam_input.sh")
         decky_plugin.logger.info(f"Determined plugin_path to be {plugin_path}")
-
-        controller_rules = os.path.join(rules_dest_dir, "99-disable-steam-input_controller.rules")
-        with open(controller_rules, "w") as rules_file:
-            # echo 'KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="1", ACTION=="add", RUN+="/home/deck/.local/share/scawp/SDADSC/disable_steam_input.sh disable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"' | sudo tee -a "$rules_install_dir/99-disable-steam-input.rules"
-            # echo 'KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="1", ACTION=="remove", RUN+="/home/deck/.local/share/scawp/SDADSC/disable_steam_input.sh enable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"' | sudo tee -a "$rules_install_dir/99-disable-steam-input.rules"
-            rules_file.write('KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="1", ACTION=="add", RUN+="\''+script_path+'\' disable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"')
-            rules_file.write("\n")
-            rules_file.write('KERNEL=="input*", SUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="1", ACTION=="remove", RUN+="\''+script_path+'\' enable %k %E{NAME} %E{UNIQ} %E{PRODUCT}"')
-            rules_file.write("\n")
-        decky_plugin.logger.info(f"Wrote steam input controller rules to {controller_rules}")
-
-        os.system("udevadm control --reload")
-        decky_plugin.logger.info("Reloaded services")
 
         os.chmod(script_path, 0o555)
         decky_plugin.logger.info("Set permissions on scawp's script")
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,94 @@
 import {
   definePlugin,
   PanelSection,
+  PanelSectionRow,
   ServerAPI,
   staticClasses,
+  ToggleField,
 } from "decky-frontend-lib";
-import { VFC } from "react";
+import { useState, VFC } from "react";
 import { FaGamepad } from "react-icons/fa";
 
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({serverAPI}) => {
+  const [isControllerModeOn, setControllerMode] = useState<boolean>(true);
+  const [isChangingControllerMode, setChangingControllerMode] = useState<boolean>(true);
+  const [isKeyboardModeOn, setKeyboardMode] = useState<boolean>(true);
+  const [isChangingKeyboardMode, setChangingKeyboardMode] = useState<boolean>(true);
+  const [isMouseModeOn, setMouseMode] = useState<boolean>(true);
+  const [isChangingMouseMode, setChangingMouseMode] = useState<boolean>(true);
+
+  serverAPI.callPluginMethod<{}, boolean>('get_state', {}).then((response) => {
+    if(response.success) {
+      setControllerMode(response.result[0]);
+      setKeyboardMode(response.result[1]);
+      setMouseMode(response.result[2]);
+      setChangingControllerMode(false);
+      setChangingKeyboardMode(false);
+      setChangingMouseMode(false);
+    }
+  });
+
   return (
-    <PanelSection title="Panel Section">
-      Controller Dock is installed!
-      The built-in Steam Controller will now be disabled whenever an external controller is connected.
-      If the external controller is disconnected, the built-in Steam Controller will be enabled again.
+    <PanelSection title="Controls">
+      <PanelSectionRow>
+        The built-in Steam Controller will now be disabled whenever one of the selected types of devices are connected.
+        If the external device is disconnected, the built-in Steam Controller will be enabled again.
+      </PanelSectionRow>
+      <ToggleField
+        label="Controller"
+        checked={isControllerModeOn}
+        onChange={(value: boolean) => {
+          if(isChangingControllerMode) return;
+          setChangingControllerMode(true);
+
+          const methodName = value ? 'enable_functionality' : 'disable_functionality';
+          serverAPI.callPluginMethod(methodName, { functionality: "controller" }).then((response) => {
+            if(response.success) setChangingControllerMode(value)
+          }).finally(() => {
+            setChangingControllerMode(false);
+          });
+        }}
+        disabled={isChangingControllerMode}
+      >
+      </ToggleField>
+      
+      <ToggleField
+        label="Keyboard"
+        checked={isKeyboardModeOn}
+        onChange={(value: boolean) => {
+          if(isChangingKeyboardMode) return;
+          setChangingKeyboardMode(true);
+
+          const methodName = value ? 'enable_functionality' : 'disable_functionality';
+          serverAPI.callPluginMethod(methodName, { functionality: 'keyboard' }).then((response) => {
+            if(response.success) setChangingKeyboardMode(value)
+          }).finally(() => {
+            setChangingKeyboardMode(false);
+          });
+        }}
+        disabled={isChangingKeyboardMode}
+      >
+      </ToggleField>
+
+      <ToggleField
+        label="Mouse"
+        checked={isMouseModeOn}
+        onChange={(value: boolean) => {
+          if(isChangingMouseMode) return;
+          setChangingMouseMode(true);
+
+          const methodName = value ? 'enable_functionality' : 'disable_functionality';
+          serverAPI.callPluginMethod(methodName, { functionality: 'mouse' }).then((response) => {
+            if(response.success) setChangingMouseMode(value)
+          }).finally(() => {
+            setChangingMouseMode(false);
+          });
+        }}
+        disabled={isChangingMouseMode}
+      >
+      </ToggleField>
+
     </PanelSection>
   );
 };
